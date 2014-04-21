13fd5d7be650cc307ab0bfa0429c550d6c838763
Timothy Martinez <tmartinez@umail.ucsb.edu>
2014-04-20 19:22:55 -0700

    Added .py Files
    -ChangeCheck (checks for certain changes to projects)
    -ProjectClasses(a list of classes for the project)
    -getClasses(Generates a list of classes from starting project files)

---
+++ changeCheck.py
@@ -0,0 +1,98 @@
+from kelp.octopi import OctopiPlugin
+import kurt
+import sys
+import os
+from optparse import OptionParser
+import projectClasses #imports classfile that contains all required classes
+
+
+classes = {"AnimalRace":projectClasses.AnimalRace,"CAGeography":projectClasses.CAGeography,
+            "CAGeographyBroadcast":projectClasses.CAGeographyBroadcast,"DanceParty":projectClasses.DanceParty,"EgyptGame":projectClasses.EgyptGame,
+            "GoldRush":projectClasses.GoldRush,"IntroGame":projectClasses.IntroGame,"MammalsGame":projectClasses.MammalsGame,"Missions":projectClasses.Missions,
+            "MissionsBroadcast":projectClasses.MissionsBroadcast,"Planets":projectClasses.Planets,"PlanetsBroadcast":projectClasses.PlanetsBroadcast,
+            "PlantGrowing":projectClasses.PlantGrowing,"RaceGame":projectClasses.RaceGame,"ThanksgivingGame":projectClasses.ThanksgivingGame} #lengthy list of classes
+def main():
+    parser = OptionParser(usage = "changeCheck.py Module [Filepath]")
+    parser.add_option('-s', '--student-dir', action="store", type="string", dest="student", default=False,
+                      help=('Analyze a directory of a projects of the same type.'))
+    opt, argv = parser.parse_args()
+    if len(argv) == 2: #analyze a single scratch file
+        module = argv[0]
+        targetPath = argv[1]
+        analyzeSingleFile(module, targetPath)
+    if opt.student: #analyze multiple scratch files (directory)
+        module = argv[0]
+        targetPath = opt.student
+        analyzeMultipleSnaps(module, targetPath)
+    elif len(argv) < 2:
+        print("usage: changeCheck.py Module (options) [Filepath]")
+        return 1
+
+    
+def analyze(module, targetPath): #takes module argument and path to a scratch file
+    oct = kurt.Project.load(os.path.abspath(targetPath))
+    l = classes[module]() #creates a class of the appropriate type based on project
+    results = []
+    if (len(oct.sprites)< l.spriteCount): #checks for sprite addition/deletion
+        results.append(str(l.spriteCount-len(oct.sprites))+" sprite(s) deleted")
+    if (len(oct.sprites) == l.spriteCount):
+        results.append("Correct amount of sprites")
+    if (len(oct.sprites) > l.spriteCount):
+        results.append(str(len(oct.sprites)-l.spriteCount)+" extra sprite(s)")
+    for spriteName, spriteTuple in l.sprites.items(): #checks all scripts against originals
+        count = 0
+        scriptText = spriteTuple[0]
+        numCostumes = spriteTuple[1]
+        for sprite in oct.sprites:
+            if spriteName == sprite.name: #start checking scripts to ensure accuracy
+                for script in sprite.scripts:
+                    for stuff in scriptText: # for each script belonging to the sprite
+                        if stuff in script.stringify(): #loop that checks for each original script
+                            count += 1
+                            if stuff == script.stringify():
+                                results.append (spriteName + ": Script " +str(count)+" unchanged")
+                            else:
+                                if (l.addOkay == False):
+                                    results.append (spriteName +":Script " +str(count)+" Additions Made")
+                            if (len (l.sprites[spriteName][0])!=0):
+                                l.sprites[spriteName][0].remove(stuff)
+                if (len(sprite.costumes) < numCostumes): #checks to see if costumes are created or deleted
+                    results.append(spriteName +":Costumes Deleted")
+                if (len(sprite.costumes)  == numCostumes):
+                    results.append(spriteName+":Same number of costumes")
+                if (len(sprite.costumes) > numCostumes):
+                    results.append(spriteName +"Costumes Added")
+    c = classes[module]()
+    for sprite, script in l.sprites.items():
+        if(len(script[0]) != 0 and (len(c.sprites[sprite][0]) != 0)):
+            results.append(sprite + ":Script deletions made or script removed")
+    return results    
+def analyzeSingleFile(module, targetFile):
+    resultsDict = {}
+    if targetFile.endswith(".oct"):
+        resultsDict[targetFile] = analyze(module, targetFile)
+    else: 
+        return
+    html_out(resultsDict)
+def analyzeMultipleSnaps(module, targetDirectory): #runs check against all files in a directory (snapshot)
+    resultsDict = {}
+    for scratchFile in os.listdir(targetDirectory):
+        if scratchFile.endswith(".oct"):
+            resultsDict[scratchFile] = analyze(module, targetDirectory+'\\'+scratchFile)
+    html_out(resultsDict)
+def html_out(resultsDict):
+    if not os.path.exists('results'):
+        os.mkdir('results')
+    html = []
+    if not os.path.exists('results/output.html'):
+        for project, list in resultsDict.items():
+            html.append("<h2>" + project + "</h2>")
+            for item in list:
+                html.append("<p>"+item +"<p>")
+        with open('results/output.html', 'w') as fp:
+            fp.write(''.join(html))
+    #write out all results to a single page. 
+
+
+if __name__ == "__main__":
+    main()
---
+++ getClasses.py
@@ -0,0 +1,52 @@
+from kelp.octopi import OctopiPlugin
+from optparse import OptionParser
+import kurt
+import sys
+import os
+
+def main():
+    parser = OptionParser(usage = "getClasses.py [DIRECTORY]") #Reads all scratch files in a directory and generates an import list for them
+    opt, argv = parser.parse_args()
+    writeList = []
+    importList = []
+    spriteCostumes = {}
+    if len(argv) == 0:
+        print("Usage: getClasses.py [Directory or File]")
+        return 1
+    for scratchfile in os.listdir(argv[0]):
+        if(scratchfile.endswith(".oct")):
+            spriteList = {}
+            spriteCount = 0
+            oct = kurt.Project.load(os.path.abspath(scratchfile))
+            spriteCount = len(oct.sprites)
+            writeList.append("\nclass " + scratchfile[0:-4] +"():\n")
+            importList.append('"'+scratchfile[0:-4]+'":projectClasses.'+scratchfile[0:-4])
+            writeList.append('    spriteCount='+str(spriteCount)+'\n')
+            writeList.append('    sprites={')
+            spriteCount = len(oct.sprites)
+            for sprite in oct.sprites:
+                costumeCount = len(sprite.costumes)
+                spriteList[sprite.name] = ([],[])
+                spriteCostumes[sprite.name] = costumeCount
+                if len(sprite.scripts) != 0:
+                    for script in sprite.scripts:
+                        spriteList[sprite.name][0].append(repr(script.stringify()))
+            for name, lister in spriteList.items():
+                writeList.append('"'+name+'":[[')
+                writeList.append(',\n    '.join((lister[0])))
+                writeList.append(']')
+                writeList.append(',' +str(spriteCostumes[name])+'],\n    ')
+            writeList.append('}')
+            writeList[-2]= writeList[-2][0:-6]
+            if (name == "AnimalRace" or "DanceParty"):
+                writeList.append('\naddOkay=True')
+            else:
+                writeList.append('\n    addOkay=False')
+    if not os.path.exists('projectClasses.py'):
+        with open ('projectClasses.py', "w") as fp:
+            fp.write(''.join(writeList))
+    with open (argv[0]+'import.txt', "w") as fp:
+        fp.write('{' + ','.join(importList)+'}')
+        
+if __name__ == "__main__":
+    main()
